615e21bbc7e612f633482583d7658d2abf2d64aa
Craig Franklin <craigjfranklin@gmail.com>
2020-09-24 20:40:52 +1000

    Bump version for new functionality

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="candystore",
-    version="0.2.0",
+    version="0.2.1",
     author="Craig Franklin",
     author_email="craigjfranklin@gmail.com",
     description="Factories for randomised AFL data sets for testing purposes",
