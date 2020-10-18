c7ccd2fbda661ea3020ea339b459f11188182bd3
Craig Franklin <craigjfranklin@gmail.com>
2020-10-18 19:43:45 +1100

    Autoformat & fix linting for conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,5 @@
+# pylint: disable=invalid-name,missing-module-docstring,redefined-builtin
+
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -17,12 +19,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'candystore'
-copyright = '2020, Craig Franklin'
-author = 'Craig Franklin'
+project = "candystore"
+copyright = "2020, Craig Franklin"
+author = "Craig Franklin"
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.1'
+release = "0.2.1"
 
 
 # -- General configuration ---------------------------------------------------
@@ -34,7 +36,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -49,9 +51,9 @@ master_doc = "index"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
