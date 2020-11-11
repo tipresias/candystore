a18b91ea1dcd7540c6dceefdd8387410a889594a
Craig Franklin <craigjfranklin@gmail.com>
2020-11-11 14:35:08 +1100

    Break betting odds data out from main CandyStore class

--- candystore/base_data.py
+++ candystore/base_data.py
@@ -13,34 +13,11 @@ import numpy as np
 import pandas as pd
 
 from .fixtures import convert_to_fixtures, FixtureData
+from .betting_odds import convert_to_betting_odds, BettingData
 
 
 SeasonRange = Tuple[int, int]
 
-
-BettingData = TypedDict(
-    "BettingData",
-    {
-        "date": datetime,
-        "season": int,
-        "round": int,
-        "home_team": str,
-        "away_team": str,
-        "home_score": int,
-        "away_score": int,
-        "home_margin": int,
-        "away_margin": int,
-        "home_win_odds": float,
-        "away_win_odds": float,
-        "home_win_paid": float,
-        "away_win_paid": float,
-        "home_line_odds": float,
-        "away_line_odds": float,
-        "home_line_paid": float,
-        "away_line_paid": float,
-        "venue": str,
-    },
-)
 
 MatchData = TypedDict(
     "MatchData",
@@ -165,8 +142,6 @@ FINALS_ROUND_LABELS = ["QF", "EF", "SF", "PF", "GF"]
 
 # Reasonable ranges are two standard deviations plus/minus from the means
 # for all recorded AFL matches
-REASONABLE_SCORE_RANGE = (23, 148)
-REASONABLE_MARGIN_RANGE = (0, 89)
 REASONABLE_GOAL_RANGE = (2, 23)
 REASONABLE_BEHIND_RANGE = (3, 22)
 # Uses minimum attendance, because standard deviation was too large
@@ -197,11 +172,6 @@ REASONABLE_TIME_ON_GROUND_RANGE = (0, 116)
 
 SUBSTITUTE_RANGE = (0, 3)
 BROWNLOW_VOTES_RANGE = (0, 4)
-
-# Roughly the payout when win odds are even
-BASELINE_BET_PAYOUT = 1.92
-# Hand-wavy math to get vaguely realistic win odds
-WIN_ODDS_MULTIPLIER = 0.8
 
 N_PLAYERS_PER_TEAM = 22
 
@@ -409,7 +379,7 @@ class CandyStore:
                 "away_line_paid": 1.92
             }
         """
-        betting_odds_data_frame = self._base_matches.pipe(self._convert_to_betting_odds)
+        betting_odds_data_frame = self._base_matches.pipe(convert_to_betting_odds)
 
         return (
             betting_odds_data_frame
@@ -551,42 +521,6 @@ class CandyStore:
         return (
             player_data_frame if to_dict is None else player_data_frame.to_dict(to_dict)
         )
-
-    @staticmethod
-    def _convert_to_betting_odds(
-        base_match_data_frame: pd.DataFrame,
-    ) -> List[BettingData]:
-        home_score, away_score = (
-            np.random.randint(*REASONABLE_SCORE_RANGE, size=len(base_match_data_frame)),
-            np.random.randint(*REASONABLE_SCORE_RANGE, size=len(base_match_data_frame)),
-        )
-        home_line_odds = np.random.randint(
-            *REASONABLE_MARGIN_RANGE, size=len(base_match_data_frame)
-        )
-        win_odds_diff = np.round(
-            (np.random.rand(len(base_match_data_frame)) * WIN_ODDS_MULTIPLIER),
-            decimals=2,
-        )
-        home_win_odds_diff = np.where(
-            home_line_odds > 0, win_odds_diff, -1 * win_odds_diff
-        )
-        home_win_odds = BASELINE_BET_PAYOUT + home_win_odds_diff
-        away_win_odds = BASELINE_BET_PAYOUT - home_win_odds_diff
-
-        return base_match_data_frame.assign(
-            home_score=home_score,
-            away_score=away_score,
-            home_margin=home_score - away_score,
-            away_margin=away_score - home_score,
-            home_win_odds=home_win_odds,
-            away_win_odds=away_win_odds,
-            home_win_paid=home_win_odds * (home_score > away_score).astype(int),
-            away_win_paid=away_win_odds * (away_score > home_score).astype(int),
-            home_line_odds=home_line_odds,
-            away_line_odds=home_line_odds * -1,
-            home_line_paid=BASELINE_BET_PAYOUT * (home_score > away_score).astype(int),
-            away_line_paid=BASELINE_BET_PAYOUT * (away_score > home_score).astype(int),
-        ).astype({"date": str})
 
     def _convert_to_matches(
         self, base_match_data_frame: pd.DataFrame
