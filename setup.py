45c2a7cb59755c82da6bd55f436ed99d78513e36
Craig Franklin <craigjfranklin@gmail.com>
2022-01-04 09:07:27 +1100

    Bump version to 0.3.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="candystore",
-    version="0.3.3",
+    version="0.3.4",
     author="Craig Franklin",
     author_email="craigjfranklin@gmail.com",
     description="Factories for randomised AFL data sets for testing purposes",
